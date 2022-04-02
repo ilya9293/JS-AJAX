b10b1f171f00939dfdc900e894f2b83b9c3f4fc0
ilya9293 <ilya9293@ukr.net>
2022-04-02 20:51:12 +0300

    сделал разметку одной страны

--- src/js/ref.js
+++ src/js/ref.js
@@ -7,28 +7,16 @@ import { debounce } from 'lodash';
 import makeCountryMarkup from '../templating/list-countries.hbs';
 import { fetchCountries } from './fetchCountries';
 
-import { info } from '@pnotify/core';
-import '@pnotify/core/dist/PNotify.css';
-import '@pnotify/core/dist/BrightTheme.css';
-import * as Confirm from '@pnotify/confirm';
-import '@pnotify/confirm/dist/PNotifyConfirm.css';
-
 const handleCountry = () => {
-  info({
-    title: 'Button Clicked',
-    text: 'You have clicked the button. You may now complete the process of reading the notice.',
-    modules: new Map([
-      [
-        Confirm,
-        {
-          confirm: false,
-        },
-      ],
-    ]),
-  });
-  //   fetchCountries(ref.input.value)
-  //     .then(data => (ref.list.innerHTML = makeCountryMarkup(data)))
-  //     .catch(err => (ref.list.textContent = 'Ошибка! Страна не найдена'));
+  fetchCountries(ref.input.value)
+    .then(data => {
+       console.log(data);
+      if (data.length > 10) {
+        return (ref.list.textContent = 'Слишком много стран!');
+      }
+      ref.list.innerHTML = makeCountryMarkup(data);
+    })
+    .catch(err => (ref.list.textContent = 'Ошибка! Страна не найдена'));
 };
 
 ref.input.addEventListener('input', debounce(handleCountry, 500));
